5981555e518c3b521ca58af3278bfc1a2018e876
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-22 10:26:35 +0530

    roboshop

--- rs-project.groovy
+++ rs-project.groovy
@@ -4,13 +4,13 @@ folder('rs-project') {
   description('rs-project')
 }
 
-pipelineJob('rs-project/mysql') {
+pipelineJob('rs-project/frontend') {
   configure { flowdefinition ->
     flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
       'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
         'userRemoteConfigs' {
           'hudson.plugins.git.UserRemoteConfig' {
-            'url'('https://github.com/imjitthu/tfas-mysql.git')
+            'url'('https://github.com/imjitthu/tfas-frontend.git')
           }
         }
         'branches' {
