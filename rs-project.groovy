31d98612d473cf15454971f5d5d3a69462a2217b
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-22 10:18:06 +0530

    roboshop

--- rs-project.groovy
+++ rs-project.groovy
@@ -4,13 +4,34 @@ folder('rs-project') {
   description('rs-project')
 }
 
-pipelineJob('rs-project/frontend') {
+pipelineJob('rs-project/mysql') {
   configure { flowdefinition ->
     flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
       'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
         'userRemoteConfigs' {
           'hudson.plugins.git.UserRemoteConfig' {
-            'url'('https://github.com/imjitthu/tfas-frontend.git')
+            'url'('https://github.com/imjitthu/tfas-mysql.git')
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/main')
+          }
+        }
+      }
+      'scriptPath'('jenkinsfile')
+      'lightweight'(true)
+    }
+  }
+}
+
+pipelineJob('rs-project/mongo') {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://github.com/imjitthu/tfas-mongo.git')
           }
         }
         'branches' {
