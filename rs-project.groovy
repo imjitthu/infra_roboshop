62ad4c0d6ed854d2b7c3a809ff19d87c54ee7d5e
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-22 10:25:12 +0530

    roboshop

--- rs-project.groovy
+++ rs-project.groovy
@@ -24,24 +24,3 @@ pipelineJob('rs-project/mysql') {
     }
   }
 }
-
-pipelineJob('rs-project/mongo') {
-  configure { flowdefinition ->
-    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-        'userRemoteConfigs' {
-          'hudson.plugins.git.UserRemoteConfig' {
-            'url'('https://github.com/imjitthu/tfas-mongo.git')
-          }
-        }
-        'branches' {
-          'hudson.plugins.git.BranchSpec' {
-            'name'('*/main')
-          }
-        }
-      }
-      'scriptPath'('jenkinsfile')
-      'lightweight'(true)
-    }
-  }
-}
