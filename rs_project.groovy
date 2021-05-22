e50b596424135f21b7c7c8aba6744662433fb428
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-22 13:04:07 +0530

    roboshop

--- rs_project.groovy
+++ rs_project.groovy
@@ -1,9 +1,9 @@
-folder('rs-components') {
+folder('rs-components0') {
   displayName('rs-components')
   description('rs-components')
 }
 
-pipelineJob('rs-components/mongo0') {
+pipelineJob('rs-components/mongo') {
   configure { flowdefinition ->
     flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
       'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
